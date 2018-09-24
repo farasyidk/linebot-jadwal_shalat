f5bb4c3966f9829f3a3ed1804dc8851817ae189e
farasyidk <farasyid36@gmail.com>
2018-09-25 02:14:18 +0700

    first commit

---
+++ README.md
@@ -0,0 +1,6 @@
+# Line_Bot_Shalat
+Script Webhook Line Messaging API Use Heroku Or Other
+
+
+Tutorial from : https://medantechno.com/read/news/23/tutorial-membuat-bot-messaging-api-line
+PHP Unirest Script Included
---
+++ bot.php
@@ -0,0 +1,117 @@
+<?php
+
+require_once('./line_class.php');
+require_once('./unirest-php-master/src/Unirest.php');
+
+$channelAccessToken = 'EU1tPN/QaGKA489teB8ynO8BrQ5CF6vO6esiEnmxC9fhXQRaCOme0ww22007VJjMKRVq0e2yVchiExTuwoh+NMm8iBeNjD1+5huwdHq5iM8BBH5YBYS5aRH1IXUcRk+N1yrkPCt3ASafIjmIWb87AgdB04t89/1O/w1cDnyilFU='; //sesuaikan 
+$channelSecret = '9a1381f127f787333645c38c7d96ed6d';//sesuaikan
+
+$client = new LINEBotTiny($channelAccessToken, $channelSecret);
+
+$userId 	= $client->parseEvents()[0]['source']['userId'];
+$groupId 	= $client->parseEvents()[0]['source']['groupId'];
+$replyToken = $client->parseEvents()[0]['replyToken'];
+$timestamp	= $client->parseEvents()[0]['timestamp'];
+$type 		= $client->parseEvents()[0]['type'];
+
+$message 	= $client->parseEvents()[0]['message'];
+$messageid 	= $client->parseEvents()[0]['message']['id'];
+
+$profil = $client->profil($userId);
+
+$pesan_datang = explode(" ", $message['text']);
+
+$command = $pesan_datang[0];
+$options = $pesan_datang[1];
+if (count($pesan_datang) > 2) {
+    for ($i = 2; $i < count($pesan_datang); $i++) {
+        $options .= '+';
+        $options .= $pesan_datang[$i];
+    }
+}
+
+#-------------------------[Function]-------------------------#
+function shalat($keyword) {
+    $uri = "https://time.siswadi.com/pray/" . $keyword;
+
+    $response = Unirest\Request::get("$uri");
+
+    $json = json_decode($response->raw_body, true);
+    $result = "Jadwal Shalat Sekitar ";
+	$result .= $json['location']['address'];
+	$result .= "\nTanggal : ";
+	$result .= $json['time']['date'];
+	$result .= "\n\nShubuh : ";
+	$result .= $json['data']['Fajr'];
+	$result .= "\nDzuhur : ";
+	$result .= $json['data']['Dhuhr'];
+	$result .= "\nAshar : ";
+	$result .= $json['data']['Asr'];
+	$result .= "\nMaghrib : ";
+	$result .= $json['data']['Maghrib'];
+	$result .= "\nIsya : ";
+	$result .= $json['data']['Isha'];
+    return $result;
+}
+#-------------------------[Function]-------------------------#
+
+# require_once('./src/function/search-1.php');
+# require_once('./src/function/download.php');
+# require_once('./src/function/random.php');
+# require_once('./src/function/search-2.php');
+# require_once('./src/function/hard.php');
+
+//show menu, saat join dan command /menu
+if ($type == 'join' || $command == '/menu') {
+    $text = "Assalamualaikum Kakak, aku adalah bot jadwal shalat, silahkan ketik\n\n/shalat <nama tempat>\n\nnanti aku bakalan kasih tahu jam berapa waktunya shalat ^_^";
+    $balas = array(
+        'replyToken' => $replyToken,
+        'messages' => array(
+            array(
+                'type' => 'text',
+                'text' => $text
+            )
+        )
+    );
+}
+
+//pesan bergambar
+if($message['type']=='text') {
+	    if ($command == '/shalat') {
+
+        $result = shalat($options);
+        $balas = array(
+            'replyToken' => $replyToken,
+            'messages' => array(
+                array(
+                    'type' => 'text',
+                    'text' => $result
+                )
+            )
+        );
+    }
+
+}else if($message['type']=='sticker')
+{	
+	$balas = array(
+							'replyToken' => $replyToken,														
+							'messages' => array(
+								array(
+										'type' => 'text',									
+										'text' => 'Makasih Kak Stikernya ^_^'										
+									
+									)
+							)
+						);
+						
+}
+if (isset($balas)) {
+    $result = json_encode($balas);
+//$result = ob_get_clean();
+
+    file_put_contents('./balasan.json', $result);
+
+
+    $client->replyMessage($balas);
+}
+?>
